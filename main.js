8dd2b36570b352cf6bd672d0694e5101b735e6e1
d.susviela <daniel.susviela@fing.edu.uy>
2019-04-17 23:18:29 -0300

    changes to api integration

---
+++ main.js
@@ -0,0 +1,34 @@
+const app = function(){
+    const API_BASE = 'https://script.google.com/macros/s/AKfycbzlzNobHRxVJg_IKgG3lOYcg1KjucdXjyxidqq_RfHJV9Uqb6c/exec';
+    const API_KEY = 'jYpdvrsGo86POPfAITngK//KPwBD/VSsQVXN5PJ2cPU=';
+
+    const juanMoreContact = 'Error en el sitio, contactanos por instagram @Juanmoretorneos'
+    // for future use
+    function init() {
+        _writeInfo();
+    }
+//instagramAcc, usr1, usr1Plat, usr2, usr2plat,
+//mail, stream, state
+    function _writeInfo() {
+        fetch(_buildApiUrl())
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.status !== 'success') {
+                    _setNotice(json.message);
+                }
+            }).catch((error) => {
+                _setNotice(juanMoreContact);
+            })
+    }
+
+    function _buildApiUrl(){
+        let url = API_BASE;
+        url += '?key=' + API_KEY;
+
+        return url;
+    }
+
+    function _setNotice (label) {
+		page.notice.innerHTML = label;
+    }
+}();
