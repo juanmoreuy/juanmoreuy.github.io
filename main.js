4dfa0d297beb3f65d21f5305577a1e355eea64fa
d.susviela <daniel.susviela@fing.edu.uy>
2019-04-18 00:09:46 -0300

    button now writes random data to sheets

--- main.js
+++ main.js
@@ -1,40 +1,34 @@
-const app = function(){
-    const API_BASE = 'https://script.google.com/macros/s/AKfycbzlzNobHRxVJg_IKgG3lOYcg1KjucdXjyxidqq_RfHJV9Uqb6c/exec';
-    const API_KEY = 'jYpdvrsGo86POPfAITngK//KPwBD/VSsQVXN5PJ2cPU=';
+const API_BASE = 'https://script.google.com/macros/s/AKfycbzlzNobHRxVJg_IKgG3lOYcg1KjucdXjyxidqq_RfHJV9Uqb6c/exec';
+const API_KEY = 'jYpdvrsGo86POPfAITngK//KPwBD/VSsQVXN5PJ2cPU=';
 
-    const juanMoreContact = 'Error en el sitio, contactanos por instagram @Juanmoretorneos'
-    // for future use
-    function init() {
-        _writeInfo();
-    }
+const juanMoreContact = 'Error en el sitio, contactanos por instagram @Juanmoretorneos'
+// for future use
+function init() {
+    _writeInfo();
+}
 //instagramAcc, usr1, usr1Plat, usr2, usr2plat,
 //mail, stream, state
-    function _writeInfo() {
-        console.log('tring to write data');
-        fetch(_buildApiUrl())
-            .then((response) => response.json())
-            .then((json) => {
-                if (json.status !== 'success') {
-                    _setNotice(json.message);
-                }
-                console.log(json.message);
-            }).catch((error) => {
-                _setNotice(juanMoreContact);
-            })
-    }
+function _writeInfo() {
+    console.log('tring to write data');
+    fetch(_buildApiUrl())
+        .then((response) => response.json())
+        .then((json) => {
+            if (json.status !== 'success') {
+                _setNotice(json.message);
+            }
+            console.log(json.message);
+        }).catch((error) => {
+            _setNotice(juanMoreContact);
+        })
+}
 
-    function _buildApiUrl(){
-        let url = API_BASE;
-        url += '?key=' + API_KEY;
+function _buildApiUrl(){
+    let url = API_BASE;
+    url += '?key=' + API_KEY;
 
-        return url;
-    }
+    return url;
+}
 
-    function _setNotice (label) {
-		page.notice.innerHTML = label;
-    }
-
-    return {
-		init: init
-    };
-}();
+function _setNotice (label) {
+    page.notice.innerHTML = label;
+}
